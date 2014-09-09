315a104a1784c49f5b2ea46e3f4686865453a7f1
goodnowt <goodnowt@lafayette.edu>
2014-09-09 17:02:44 -0400

    added missing authorial statement

--- js/src/human_form.js
+++ js/src/human_form.js
@@ -15,6 +15,10 @@
 	     */
 	    //$('[name="field_human_pers_rels_add_more"]').parents('.form-item').prepend($('[name="field_human_pers_rels_add_more"]').parent().detach().show());
 	    
+	    /*
+	     * @author goodnowt function to detect whether form has been loaded after "save & add another" was clicked, and add classes to mark-up accordingly
+	     */
+	    
 	    if (document.getElementsByClassName('alert alert-block alert-success').length == 1){
 			
 			$('#edit-field-human-surname-und-0-value').addClass('prepopped');
